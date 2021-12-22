0c7eaf8a9bc842d988f119195bca75118e9677bc
tzipor <50782805+tzipor@users.noreply.github.com>
2021-12-22 17:17:03 -0500

    Proper implementation of scrape.py, to be run daily in production after daily deletion of all rows from job table

--- scrape.py
+++ scrape.py
@@ -2,17 +2,34 @@ import requests
 import indeed
 import glassdoor
 
-base_url = "http://127.0.0.1:5000/"
+# Run scraping functions for Indeed and Glassdoor using each query in the database, then post jobs to database
+def main():
+    base_url = "http://127.0.0.1:5000/"
+    queryResponseRaw = requests.get(base_url + "query")
+    all_queries = queryResponseRaw.json()
 
-queryResponseRaw = requests.get(base_url + "query")
+    i = 1
+    j = len(all_queries)
+    
+    for query_item in all_queries:
+        print(f'Scraping Indeed for query {i} of {j}...')
+        jobs_list_indeed = indeed.iJobs(**query_item).get()
+        print(f'Scraping Glassdoor for query {i} of {j}...')
+        jobs_list_glassdoor = glassdoor.gJobs(**query_item).get()
 
-all_queries = queryResponseRaw.json()
+        jobs_url = base_url + "job"
 
-test_query = all_queries[0]
+        for i_job in jobs_list_indeed:
+            print(f'Posting Indeed jobs for query {i} of {j}...')
+            send_job = requests.post(jobs_url, json=i_job)
 
-jobs_list_test = glassdoor.gJobs(**test_query).get()
+        for g_job in jobs_list_glassdoor:
+            print(f'Posting Glassdoor jobs for query {i} of {j}...')
+            send_job = requests.post(jobs_url, json=g_job)
 
-for job in jobs_list_test:
-    print(f'Sending:\n{job}')
-    full_url = base_url + "job"
-    send_job = requests.post(full_url, json=job)
+        i += 1
+    
+    print("All jobs posted for this cycle")
+
+if __name__ == '__main__':
+    main()
