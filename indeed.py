8a865df75bb5beabb12f02f1d9cc33f2ef3a8a54
tzipor <50782805+tzipor@users.noreply.github.com>
2021-12-22 16:08:01 -0500

    Removed unused imports

--- indeed.py
+++ indeed.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup as bs
